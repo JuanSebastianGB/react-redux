5f7eb10aafe52ec240b39b51c78505a6539fcbad
JuanSebastinGB <3393@holbertonschool.com>
2022-06-20 19:04:06 -0500

    Adding Button to clear cart

--- src/components/shoppingcart/ShoppingCart.js
+++ src/components/shoppingcart/ShoppingCart.js
@@ -1,15 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProductItem from '../productitem/ProductItem';
 import ProductCart from '../productcart/ProductCart';
+import { clearCart } from '../../actions/shoppingCartActions';
 
 const ShoppingCart = () => {
   const state = useSelector(state => state);
+  const dispatch = useDispatch();
   const { shoppingCart } = state;
   console.log(shoppingCart.products);
   return (
     <div>
       <h2>ShoppingCart</h2>
       <h3>Products</h3>
+      <button onClick={() => dispatch(clearCart())}>Clear Cart</button>
       <article className='box grid-responsive'>
         {shoppingCart.products.map(data => (
           <ProductItem
